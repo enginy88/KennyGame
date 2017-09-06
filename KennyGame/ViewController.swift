7b516c50b027762b733d066cce13955f7a2801be
Engin YÜCE <enginy88@gmail.com>
2017-09-06 10:37:09 +0300

    Target change, function reordering.

--- KennyGame/ViewController.swift
+++ KennyGame/ViewController.swift
@@ -29,37 +29,76 @@ class ViewController: UIViewController {
     var pictSize: CGFloat = 0
     
     var pictPost: UInt32 = 0
-    
-    func startTimer() {
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.timerFunc), userInfo: nil, repeats: true)
-    }
-    
-    func resetCurrentGame() {
-        pictPost = 0
-        currentScore = 0
-        bestLabel.text = "Best: " + String(bestScore)
-        scoreLabel.text = "Score: 0"
-        remainingLabel.text = "Remaining: 0"
-        progressView.progress = 0.0
+
+    @IBAction func resetButtonTouched(_ sender: Any) {
+
         timer.invalidate()
-        
-        imageView.isHidden = true
-        startButton.isHidden = false
+        resetAlert(title: "Reset Which One?", message: "Please Select One:")
+
     }
-    
-    func clearBestScore(){
-        bestScore = 0
+
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+
+        winWidth = view.frame.size.width
+        winHeight = view.frame.size.height
+        pictSize = winWidth * winHeight * 0.0004
+
         startButton.setTitle("Start Game!", for: UIControlState.normal)
-        
-        if (UserDefaults.standard.object(forKey: "bestScore") as? Int) != nil {
-            UserDefaults.standard.removeObject(forKey: "bestScore")
+        startButton.setTitleColor(self.view.tintColor, for: UIControlState.normal)
+        startButton.frame = CGRect.init(x: 0, y: winHeight * 0.5, width: winWidth, height: 20)
+        startButton.addTarget(self, action: #selector(startButtonTouched), for: UIControlEvents.touchUpInside)
+
+        imageView.image = UIImage(named: "Kenny.jpg")
+        imageView.contentMode = UIViewContentMode.scaleAspectFit
+        imageView.isUserInteractionEnabled = true
+        imageView.frame = CGRect.init(x: (winWidth - pictSize) / 2, y: (winHeight - pictSize) / 2, width: pictSize, height: pictSize)
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.pictureTouched)))
+
+        imageView.isHidden = true
+        startButton.isHidden = true
+
+        view.addSubview(imageView)
+        view.addSubview(startButton)
+
+        if let bestScore = UserDefaults.standard.object(forKey: "bestScore") as? Int {
+            self.bestScore = bestScore
         }
-        UserDefaults.standard.synchronize()
-        
+        else {
+            bestScore = 0
+        }
+
         resetCurrentGame()
+
+    }
+
+    @objc func startButtonTouched(_ sender: Any) {
+
+        counter = MAX_COUNT
+        remainingLabel.text = "Remaining: " + String(counter)
+        progressView.progress = 1.0
+
+        imageView.isHidden = false
+        startButton.isHidden = true
+
+        startTimer()
+    }
+
+    @objc func timerFunc(_ sender: Any) {
+
+        counter -= 1
+
+        remainingLabel.text = "Remaining: " + String(counter)
+        progressView.progress = Float(counter) / Float(MAX_COUNT)
+        if counter <= 0 {
+            endGame()
+        }
+
     }
     
     @objc func pictureTouched() {
+
         currentScore += 1
         scoreLabel.text = "Score: " + String(currentScore)
         
@@ -98,44 +137,44 @@ class ViewController: UIViewController {
         }
 
     }
-    
-    override func viewDidLoad() {
-        
-        super.viewDidLoad()
-        
-        winWidth = view.frame.size.width
-        winHeight = view.frame.size.height
-        pictSize = winWidth * winHeight * 0.0004
-        
-        startButton.setTitle("Start Game!", for: UIControlState.normal)
-        startButton.setTitleColor(self.view.tintColor, for: UIControlState.normal)
-        startButton.frame = CGRect.init(x: 0, y: winHeight * 0.5, width: winWidth, height: 20)
-        startButton.addTarget(self, action: #selector(startButtonTouched), for: UIControlEvents.touchUpInside)
-        
-        imageView.image = UIImage(named: "Kenny.jpg")
-        imageView.contentMode = UIViewContentMode.scaleAspectFit
-        imageView.isUserInteractionEnabled = true
-        imageView.frame = CGRect.init(x: (winWidth - pictSize) / 2, y: (winHeight - pictSize) / 2, width: pictSize, height: pictSize)
-        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.pictureTouched)))
+
+    func startTimer() {
+
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.timerFunc), userInfo: nil, repeats: true)
+
+    }
+
+    func resetCurrentGame() {
+
+        pictPost = 0
+        currentScore = 0
+        bestLabel.text = "Best: " + String(bestScore)
+        scoreLabel.text = "Score: 0"
+        remainingLabel.text = "Remaining: 0"
+        progressView.progress = 0.0
+        timer.invalidate()
 
         imageView.isHidden = true
-        startButton.isHidden = true
-        
-        view.addSubview(imageView)
-        view.addSubview(startButton)
-        
-        if let bestScore = UserDefaults.standard.object(forKey: "bestScore") as? Int {
-            self.bestScore = bestScore
+        startButton.isHidden = false
+
+    }
+
+    func clearBestScore(){
+
+        bestScore = 0
+        startButton.setTitle("Start Game!", for: UIControlState.normal)
+
+        if (UserDefaults.standard.object(forKey: "bestScore") as? Int) != nil {
+            UserDefaults.standard.removeObject(forKey: "bestScore")
         }
-        else {
-            bestScore = 0
-        }
-        
+        UserDefaults.standard.synchronize()
+
         resetCurrentGame()
 
     }
-    
+
     func endGame(){
+
         startButton.setTitle("Score: " + String(currentScore) + ", Start Again?", for: UIControlState.normal)
         
         if currentScore > bestScore {
@@ -147,17 +186,9 @@ class ViewController: UIViewController {
         }
         
         resetCurrentGame()
+
     }
-    
-    @objc func timerFunc(_ sender: Any) {
-        counter -= 1
-        
-        remainingLabel.text = "Remaining: " + String(counter)
-        progressView.progress = Float(counter) / Float(MAX_COUNT)
-        if counter <= 0 {
-            endGame()
-        }
-    }
+
     
     func resetAlert (title: String, message: String) {
         
@@ -177,22 +208,7 @@ class ViewController: UIViewController {
         
     }
     
-    @IBAction func resetButtonTouched(_ sender: Any) {
-        timer.invalidate()
-        resetAlert(title: "Reset Which One?", message: "Please Select One:")
-    }
-    
-    @objc func startButtonTouched(_ sender: Any) {
-     
-        counter = MAX_COUNT
-        remainingLabel.text = "Remaining: " + String(counter)
-        progressView.progress = 1.0
-        
-        imageView.isHidden = false
-        startButton.isHidden = true
-        
-        startTimer()
-    }
+
 
 }
 
